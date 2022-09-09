eb36c05cb104d86f958f611217d9420671633819
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-09 02:21:45 +0200

    base

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import ntpath
 import numpy as np
 import pygame
 import sys
 import math
+
+
+def create_board():
+    board = np.zeros((6, 7))
+    return board
+
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask player 1 for input
+    if turn == 0:
+        slection = input("Player 1 Make your selection (o-6):")
