c43b2d14e109f1f8c12101072db335d6700e492b
Nikolina-coder <nikolina.lekaj12@gmail.com>
2022-09-10 02:24:21 +0200

    final

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import ntpath
-from operator import truediv
-from pickle import TRUE
-from select import select
-from tkinter.tix import ROW
-from turtle import width
 import numpy as np
 import pygame
 import sys
 import math
 
-
-# rgb color
-GREY = (128, 128, 128)  # BOJA POZADINE
-ROSEGOLD = (183, 136, 110)  # IGRAC 2
-BLACK = (0, 0, 0)  # IGRAC 1
-WHITE = (255, 255, 255)  # PRAZNO POLJE
-
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
-
-# FUNKCIJE
 
 
 def create_board():
@@ -36,36 +25,36 @@ def is_valid_location(board, col):
     return board[ROW_COUNT-1][col] == 0
 
 
-def get_next_open_row(board, col):  # ako nam je polje prazno prikazuje 0
+def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
         if board[r][col] == 0:
             return r
 
 
-def print_board(board):  # izmjena redosljeda, printanje od dole prema gore
+def print_board(board):
     print(np.flip(board, 0))
 
 
 def winning_move(board, piece):
-    # pregled svih horizontalnih lokacija za pobjedu
+    # Check horizontal locations for win
     for c in range(COLUMN_COUNT-3):
         for r in range(ROW_COUNT):
             if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
                 return True
 
-    # pegled svih vertikalnih lokacija za pobjedu
+    # Check vertical locations for win
     for c in range(COLUMN_COUNT):
-        for r in range(ROW_COUNT - 3):
+        for r in range(ROW_COUNT-3):
             if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
                 return True
 
-    # pregled dijagonale s pozitivnim nagibom (lijevo na desno)
+    # Check positively sloped diaganols
     for c in range(COLUMN_COUNT-3):
         for r in range(ROW_COUNT-3):
             if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
                 return True
 
-    # pregled dijagonale s negativnim nagibom (desno na lijevo)
+    # Check negatively sloped diaganols
     for c in range(COLUMN_COUNT-3):
         for r in range(3, ROW_COUNT):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
@@ -75,20 +64,19 @@ def winning_move(board, piece):
 def draw_board(board):
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
-            pygame.draw.rect(
-                screen, GREY, (c*SQUARESIZE, r * SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
-            pygame.draw.circle(
-                screen, WHITE, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), radius)
+            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r *
+                             SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2),
+                               int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
 
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
             if board[r][c] == 1:
-                pygame.draw.circle(
-                    screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), height - int(r*SQUARESIZE+SQUARESIZE/2)), radius)
+                pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2),
+                                   height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
             elif board[r][c] == 2:
-                pygame.draw.circle(
-                    screen, ROSEGOLD, (int(c*SQUARESIZE+SQUARESIZE/2), height - int(r*SQUARESIZE+SQUARESIZE/2)), radius)
-
+                pygame.draw.circle(screen, YELLOW, (int(
+                    c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
     pygame.display.update()
 
 
@@ -97,20 +85,22 @@ print_board(board)
 game_over = False
 turn = 0
 
-# Link to Pygame Documentation: https: // www.pygame.org/docs/
 pygame.init()
+
 SQUARESIZE = 100
 
 width = COLUMN_COUNT * SQUARESIZE
 height = (ROW_COUNT+1) * SQUARESIZE
 
 size = (width, height)
-radius = int(SQUARESIZE/2 - 5)
+
+RADIUS = int(SQUARESIZE/2 - 5)
 
 screen = pygame.display.set_mode(size)
 draw_board(board)
 pygame.display.update()
 
+myfont = pygame.font.SysFont("monospace", 75)
 
 while not game_over:
 
@@ -118,41 +108,53 @@ while not game_over:
         if event.type == pygame.QUIT:
             sys.exit()
 
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+            posx = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(
+                    screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+            else:
+                pygame.draw.circle(
+                    screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+        pygame.display.update()
+
         if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
             # print(event.pos)
-            # Ask player 1 for input
+            # Ask for Player 1 Input
             if turn == 0:
                 posx = event.pos[0]
                 col = int(math.floor(posx/SQUARESIZE))
-
-                # if turn == 0:
-                # col = int(input("Player 1 Make your selection (0-6):"))
 
                 if is_valid_location(board, col):
                     row = get_next_open_row(board, col)
                     drop_piece(board, row, col, 1)
 
                     if winning_move(board, 1):
-                        print("PLAYER 1 WINS!")
+                        label = myfont.render("Player 1 wins!!", 1, RED)
+                        screen.blit(label, (40, 10))
                         game_over = True
 
-                # Ask player 2 for input
+            # # Ask for Player 2 Input
             else:
                 posx = event.pos[0]
                 col = int(math.floor(posx/SQUARESIZE))
-                # col = int(input("Player 2 Make your selection (0-6):"))
 
                 if is_valid_location(board, col):
                     row = get_next_open_row(board, col)
                     drop_piece(board, row, col, 2)
 
-                    if winning_move(board, 1):
-                        print("PLAYER 2 WINS!")
+                    if winning_move(board, 2):
+                        label = myfont.render("Player 2 wins!!", 1, YELLOW)
+                        screen.blit(label, (40, 10))
                         game_over = True
 
             print_board(board)
             draw_board(board)
 
-            # izmjena izmedju prvog i drugog igraca
             turn += 1
             turn = turn % 2
+
+            if game_over:
+                pygame.time.wait(3000)
